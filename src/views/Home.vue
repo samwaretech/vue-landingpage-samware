510081a5099d383b560d3def703a38e9b6cdd3ca
fazaio <faza6028@gmail.com>
2020-06-08 21:28:09 +0700

    landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,7 +28,7 @@
     <section>
       <div class="container center" style="margin-top:-160px;padding:100px 0px;">
         <div class="card shadows" style="border-radius:10px">
-          <div style="font-size:23px;padding:20px 0px;">
+          <div style="font-size:23px;padding:20px 0px;color:#ec0f72">
             <b>Layanan Samware</b>
           </div>
           <div class="grid-three rsv">
@@ -90,7 +90,8 @@
     </section>
     <section style="background-color:white">
       <div class="container center" style="padding:100px 0px">
-        <h2>Harga</h2>Miliki website hanya dengan Rp. 49.000 per bulan
+        <div class="ps mbs" style="color:#ec0f72"><h1>Harga Layanan</h1><span class="tgraym">Era Industri 4.0 sudah datang loh! yakin kamu gak ketinggalan?</span></div>
+        
         <div class="ps left">
           <h4>Web Statis</h4>
           <table>
@@ -105,9 +106,15 @@
             </tr>
             <tr>
               <td>Domain</td>
-              <td><span class="tred">*</span>Gratis Domain</td>
-              <td><span class="tred">*</span>Gratis Domain</td>
-              <td><span class="tred">*</span>Gratis Domain</td>
+              <td>
+                <span class="tred">*</span>Gratis Domain
+              </td>
+              <td>
+                <span class="tred">*</span>Gratis Domain
+              </td>
+              <td>
+                <span class="tred">*</span>Gratis Domain
+              </td>
             </tr>
             <tr>
               <td>SSL</td>
@@ -131,31 +138,135 @@
               <td>HARGA</td>
               <td>750k/thn</td>
               <td>1150k/thn</td>
-              <td><button>Hubungi kami</button></td>
+              <td>
+                <button>Hubungi kami</button>
+              </td>
             </tr>
           </table>
         </div>
       </div>
     </section>
-    <section class="bred">
-      <div class="container twhite" style="padding:120px 0px">
-        <div class="ps mbs center"><h1>Tentang Website</h1></div>
-        <div class="card tgraym shadows" style="padding:50px 15px">
-          <div class="grid-two">
-            <div> Website Statis</div>
-            <div>Website Statis adalah sebuah website  yang cocok bagi para pemilik cafe,kedai,restaurant,minimarket untuk memasarkan dan mengoptimalkan profil mereka melalui website ringan dan instant.</div>
+    <div class="triangle" style="background-color:#ec0f72">
+      <div class="container twhite" style="padding:20px 0px">
+        <div class="grid-three">
+          <div class="ps center">
+            <div style="font-size:32px">51+</div>Project done
+          </div>
+          <div class="ps center">
+            <div style="font-size:32px">44+</div>Client
+          </div>
+          <div class="ps center">
+            <div style="font-size:32px">12+</div>Developer
           </div>
         </div>
-        <div class="card tgraym shadows" style="padding:50px 15px">
-          <div class="grid-two">
-            <div> Website Statis</div>
-            <div>Website Statis adalah sebuah website  yang cocok bagi para pemilik cafe,kedai,restaurant,minimarket untuk memasarkan dan mengoptimalkan profil mereka melalui website ringan dan instant.</div>
+      </div>
+    </div>
+    <section>
+      <div class="container center twhite" style="padding:80px 0px;color:#ec0f72 !important">
+        <div style="margin-bottom:50px">
+          <h1>Proses Pengembangan</h1>
+        <span class="tgraym">
+          Langkah mudah untuk
+          Optimalisasi bisnismu
+        </span>
+        </div>
+        <div class="grid-three rsv">
+          <div class="ps center">
+            <img width="120" src="@/assets/img/5/secfive-logo-1.png" alt="">
+            <div>
+              Analisa Kebutuhan
+            </div>
+          </div>
+          <div class="ps center">
+            <img width="120" src="@/assets/img/5/secfive-logo-2.png" alt="">
+            <div>
+              Perancangan & Pemodelan
+            </div>
+          </div>
+          <div class="ps center">
+            <img width="120" src="@/assets/img/5/secfive-logo-3.png" alt="">
+            <div>
+              Uji coba & Pengembangan
+            </div>
           </div>
         </div>
-        <div class="card tgraym shadows" style="padding:50px 15px">
-          <div class="grid-two">
-            <div> Website Statis</div>
-            <div>Website Statis adalah sebuah website  yang cocok bagi para pemilik cafe,kedai,restaurant,minimarket untuk memasarkan dan mengoptimalkan profil mereka melalui website ringan dan instant.</div>
+      </div>
+    </section>
+    <section style="background-color:#ec0f72;">
+      <div class="twhite center container" style="padding:80px 0px">
+        <h1>Tanya Jawab</h1>
+        <div class="card shadows left tgraym">
+          <span style="color:#ec0f72">Apakah Saya bisa Mengcustom permintaan?</span>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus quos amet commodi debitis deserunt, fugiat qui, obcaecati voluptatem quisquam eligendi vel nostrum suscipit repudiandae possimus nisi tempora, tenetur dolorem porro.</li>
+          </ul>
+        </div>
+        <div class="card shadows left tgraym">
+          <span style="color:#ec0f72">Apakah Saya bisa Mengcustom permintaan?</span>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus quos amet commodi debitis deserunt, fugiat qui, obcaecati voluptatem quisquam eligendi vel nostrum suscipit repudiandae possimus nisi tempora, tenetur dolorem porro.</li>
+          </ul>
+        </div>
+        <div class="card shadows left tgraym">
+          <span style="color:#ec0f72">Apakah Saya bisa Mengcustom permintaan?</span>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus quos amet commodi debitis deserunt, fugiat qui, obcaecati voluptatem quisquam eligendi vel nostrum suscipit repudiandae possimus nisi tempora, tenetur dolorem porro.</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section style="background-color:white" class="hide">
+      <div class="container center" style="padding:80px 0px">
+         <div class="ps mbs" style="color:#ec0f72"><h1>Supported By</h1></div>
+         <div class="grid-three rsv">
+           <div>
+             <div class="ps">
+               <img src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-19/s150x150/68735004_2584671218290957_5280287812233461760_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=101&_nc_ohc=SXlMQatlBM0AX9wwIiQ&oh=fe5047153628f9b45ebdab90030e3c50&oe=5F07F98C" alt="">
+             </div>
+           </div>
+           <div>
+             <div class="ps">
+               <img style="border-radius:50%" src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-19/s150x150/79693386_377890169725362_4213982517948055552_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=106&_nc_ohc=LbiC4vv_-hgAX8sIGVU&oh=fbb854c2d989b114882908364d636803&oe=5F058C42" alt="">
+             </div>
+           </div>
+           <div>
+             <div class="ps">
+               <img src="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-19/s150x150/70920744_2500434920281995_8621147802967212032_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=103&_nc_ohc=Pxp3efz7-98AX8Cn5d6&oh=e33560c8ed1ae270be2284821da3270f&oe=5F06D206" alt="">
+             </div>
+           </div>
+         </div>
+         <div class="grid-three rsv">
+           <div>
+             <div class="ps">
+               <img src="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-19/s150x150/71237206_925127214511376_2175975919875784704_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=108&_nc_ohc=IZNx2JVrpZwAX9dTYgp&oh=96f112813ac8977e75d2501b6ec7edc5&oe=5F073E67" alt="">
+             </div>
+           </div>
+           <div>
+             <div class="ps">
+               <img src="https://scontent-sin6-1.cdninstagram.com/v/t51.2885-19/s150x150/92228731_658776828243781_4232340981992652800_n.jpg?_nc_ht=scontent-sin6-1.cdninstagram.com&_nc_cat=101&_nc_ohc=R7h6H8tuKmUAX8HK-KL&oh=b6cfa46f74a03b33fd4d06af104e01fe&oe=5F05E90C" alt="">
+             </div>
+           </div>
+           <div>
+             <div class="ps">
+               <img style="border-radius:50%" src="https://scontent-sin6-2.cdninstagram.com/v/t51.2885-19/s150x150/57625238_575551579608662_7710240086217981952_n.jpg?_nc_ht=scontent-sin6-2.cdninstagram.com&_nc_cat=102&_nc_ohc=IK4mAPrV9ssAX_rqLQT&oh=aed19c78a671359936610bc9e6e233e0&oe=5F072F2D" alt="">
+             </div>
+           </div>
+         </div>
+      </div>
+    </section>
+    <section style="min-height:20vh;">
+      <div class="container" style="padding:50px 0px">
+        <div class="center ps mbs" style="color:#ec0f72"><h1>Testimoni</h1></div>
+        <div class="grid-two rsv">
+          <div class="center">
+            <img style="border-radius:50%" src="https://samware.io/home/assets/img/6/fazaio.jpg" alt="">
+          </div>
+          <div>" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore suscipit at reiciendis ut similique omnis, ab hic ea quo quibusdam praesentium, sit fugiat minus! Delectus nam voluptatibus mollitia perferendis aspernatur. "
+            <br>
+            <div class="ps">
+              <b>- Faza Aulia</b>
+              <div class="f12">Mahasiswa UMM</div>
+            </div>
           </div>
         </div>
       </div>
@@ -186,4 +297,21 @@ section {
   border-bottom-left-radius: 50% 8%;
   border-bottom-right-radius: 50% 8%;
 } */
+.triangle {
+  position: relative;
+  background: #ec0f72;
+}
+
+.triangle::before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 40px 40px 0 40px;
+  border-color: #ec0f72 transparent transparent transparent;
+  left: 50%;
+  transform: translateX(-50%) translateY(100%);
+}
 </style>
